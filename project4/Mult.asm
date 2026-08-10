1af7a038d7b6c113d5787468c5f5c126f8d4c3e4
Taiju3 <101559781+Taiju3@users.noreply.github.com>
2026-08-10 18:35:11 +0900

    Add files via upload

---
+++ project4/Mult.asm
@@ -0,0 +1,58 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+// 以下mycomment
+// ファイル：Mult.asm
+// RAM[2]にRAM[0]×RAM[1]を計算する
+// ALUに積の演算は実装されていないので、RAM[0]をRAM[1]回足し合わせることで実装してみる
+
+    // i = 0
+    @i
+    M=0
+    // sum = 0
+    @sum
+    M=0
+(LOOP)
+    // if (i = R1) goto STOP
+    @i
+    D=M
+    @R1
+    D=D-M
+    @STOP
+    D;JEQ
+    // sum = sum + R0
+    @sum
+    D=M
+    @R0
+    D=D+M
+    @sum
+    M=D
+    // i = i + 1
+    @i
+    M=M+1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(STOP)
+    // R2 = sum
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
+    
+
+
+
+
+
+
+
